fe8251d7edc78d68064e437561e9b3f347bc99b2
Bubudiep <hlitdvp98@gmail.com>
2024-09-28 17:20:31 +0700

    update link

--- core/urls.py
+++ core/urls.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('o/', include('oauth2_provider.urls', namespace='oauth2_provider')),
+    path('api/admin/', admin.site.urls),
+    path('api/o/', include('oauth2_provider.urls', namespace='oauth2_provider')),
     path('api/', include('app.urls')),  # Thay 'myapp' bằng tên ứng dụng của bạn
 ]
